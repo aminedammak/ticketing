ff0cd7dcaf1b14bb15c70e9859d276dd417153fd
adammak <aminedammak86@gmail.com>
2021-01-12 13:58:06 +0100

    Handle signout

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -7,6 +7,7 @@ router.get("/api/users/currentuser", (req, res) => {
   if (!req.session?.jwt) {
     return res.send({ currentUser: null });
   }
+  //Returns the payload if jwt is valid. If not, throw the error.
   try {
     const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
     res.send({ currentUser: payload });
