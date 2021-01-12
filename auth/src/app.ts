c0cc07280c67e929bdafb1bc416e95d0d5b0340c
adammak <aminedammak86@gmail.com>
2021-01-12 16:47:21 +0100

    Refactor app related content into seperated file

---
+++ auth/src/app.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+
+const app = express();
+app.set(
+  "trust proxy",
+  true
+); /* To make sure that express is aware the it's behind a proxy of Ingress-nginx */
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false /* Disable encryption since JWT will be already encrypted*/,
+    secure: true /* Allow cookies only for https connection */,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,40 +1,5 @@
-import express from "express";
-import "express-async-errors";
 import mongoose from "mongoose";
-import { json } from "body-parser";
-import cookieSession from "cookie-session";
-
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-app.set(
-  "trust proxy",
-  true
-); /* To make sure that express is aware the it's behind a proxy of Ingress-nginx */
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false /* Disable encryption since JWT will be already encrypted*/,
-    secure: true /* Allow cookies only for https connection */,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all("*", async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
-
+import { app } from "./app";
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
