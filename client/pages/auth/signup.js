e6a4521786c1ded085a9ba2bd38edf5d7396caef
adammak <aminedammak86@gmail.com>
2021-01-13 17:10:19 +0100

    Handle errors and show them on the signup page

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,14 +4,18 @@ import axios from "axios";
 export default () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    const response = await axios.post("/api/users/signup", {
-      email,
-      password,
-    });
-    console.log("response", response.data);
+    try {
+      const response = await axios.post("/api/users/signup", {
+        email,
+        password,
+      });
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
   };
 
   return (
@@ -34,6 +38,17 @@ export default () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Ooops...!</h4>
+          <ul className="my-0">
+            {errors.map((e, i) => (
+              <li key={i}>{e.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+
       <button type="submit" className="btn btn-primary">
         Sign In
       </button>
