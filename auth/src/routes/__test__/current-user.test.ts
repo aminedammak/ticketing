fd2f5427b597a4efcd1d754612bdc837731220ab
adammak <aminedammak86@gmail.com>
2021-01-13 13:32:25 +0100

    Added test case respond with null if not authenticated

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -10,3 +10,11 @@ it("respond with details about the current user", async () => {
     .expect(200);
   expect(response.body.currentUser.email).toEqual("test@test.com");
 });
+
+it("respond with null if not authenticated", async () => {
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(200);
+  expect(response.body.currentUser).toEqual(null);
+});
