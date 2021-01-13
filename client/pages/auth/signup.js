1a3a58a694b268b86ca3b1cd91f590f31fe30d0d
adammak <aminedammak86@gmail.com>
2021-01-13 16:31:47 +0100

    Make request to client to signup

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,14 +1,38 @@
+import { useState } from "react";
+import axios from "axios";
+
 export default () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const response = await axios.post("/api/users/signup", {
+      email,
+      password,
+    });
+    console.log("response", response.data);
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>sign up</h1>
       <div className="form-group">
         <label>Email address</label>
-        <input className="form-control" />
+        <input
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div className="form-group">
         <label>Passowrd</label>
-        <input type="password" className="form-control" />
+        <input
+          type="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
       <button type="submit" className="btn btn-primary">
         Sign In
