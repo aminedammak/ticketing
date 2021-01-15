d24ad6f73443b5551511bf70a41629d5ff17e4cb
adammak <aminedammak86@gmail.com>
2021-01-15 09:32:04 +0100

    Getting currentuser while rendering on the server side

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,29 @@
-export default () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log("currentuser", currentUser);
+
   return <h1>Landing page</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    //We are on a server
+    //Requests should be made to http://<ingress-nginx_service_name>.<ingress-nginx_namespace>.svc.cluster.local/...
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    //We are on the client!
+    //Requests can be made with base url of ""
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
