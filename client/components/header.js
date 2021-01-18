48b08fcf710bb1862ca7eb9870e4b21adcadc0f9
adammak <aminedammak86@gmail.com>
2021-01-18 11:12:38 +0100

    Implemented sign out

--- client/components/header.js
+++ client/components/header.js
@@ -1,15 +1,26 @@
 import Link from "next/link";
 
 export default function Header({ currentUser }) {
+  const links = [
+    !currentUser && { label: "Sign Up", href: "/auth/signup" },
+    !currentUser && { label: "Sign In", href: "/auth/signin" },
+    currentUser && { label: "Sign Out", href: "/auth/signout" },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => (
+      <li className="nav-item" key={href}>
+        <Link className="nav-link" href={href}>
+          <a>{label}</a>
+        </Link>
+      </li>
+    ));
   return (
     <nav className="navbar navbar-light bg-light">
       <Link href="/">
         <a className="navbar-brand">GitTix</a>
       </Link>
       <div className=" d-flex justify-content-end">
-        <ul className="nav d-flex align-items-center">
-          {currentUser ? "Sign out" : "Sign in/up"}
-        </ul>
+        <ul className="nav d-flex align-items-center">{links}</ul>
       </div>
     </nav>
   );
