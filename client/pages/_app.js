d4d4720cc09cc0f05d83313a79bcb369c3a9f9fd
adammak <aminedammak86@gmail.com>
2021-01-18 10:25:11 +0100

    Adapt Header according to the authentication state

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,16 +1,28 @@
 import "bootstrap/dist/css/bootstrap.css";
 import buildClient from "../api/build-client";
+import Header from "../components/header";
 
-const AppComponent = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 AppComponent.getInitialProps = async (appContext) => {
   const client = buildClient(appContext.ctx);
   const { data } = await client.get("/api/users/currentuser");
-  console.log("from _data", data.currentUser);
 
-  return data;
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  console.log("pageProps", pageProps);
+
+  return { pageProps, ...data };
 };
 
 export default AppComponent;
