6b0e2939b2e829d148b784d63965d0177c861140
adammak <aminedammak86@gmail.com>
2021-01-20 16:35:21 +0100

    Log errors in error-handler, generate id using mongoose helper function

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,6 +6,7 @@ import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@dmk_tickets/common";
 
 import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
 
 const app = express();
 app.set(
@@ -23,6 +24,7 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,9 +1,12 @@
+import { Mongoose } from "mongoose";
 import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/tickets";
+import mongoose from "mongoose";
 
 it("returns a 404 if the ticket is not found", async () => {
-  await request(app).get("/api/tickets/55448").send().expect(404);
+  const id = mongoose.Types.ObjectId().toHexString();
+  await request(app).get(`/api/tickets/${id}`).send().expect(404);
 });
 it("returns the ticket if the ticket is not found", async () => {
   const title = "cinema";
