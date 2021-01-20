1ed2ba9b0e1b2159bd1298ee0494a84a657dadbf
adammak <aminedammak86@gmail.com>
2021-01-20 17:28:48 +0100

    Created the list tickets route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -7,6 +7,7 @@ import { errorHandler, NotFoundError, currentUser } from "@dmk_tickets/common";
 
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
 
 const app = express();
 app.set(
@@ -25,6 +26,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -8,7 +8,7 @@ it("returns a 404 if the ticket is not found", async () => {
   const id = mongoose.Types.ObjectId().toHexString();
   await request(app).get(`/api/tickets/${id}`).send().expect(404);
 });
-it("returns the ticket if the ticket is not found", async () => {
+it("returns the ticket if the ticket is found", async () => {
   const title = "cinema";
   const price = 20;
 
