7ed2d0c21e96c6d7f199ee06bcfc0b6b4242570f
adammak <aminedammak86@gmail.com>
2021-01-21 08:56:26 +0100

    Fix typo error in  MONGO_URI

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -4,11 +4,11 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
-  if (!process.env.MONGODB_URI) {
-    throw new Error("MONGODB_URI must be defined");
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
   }
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       /* Those options are needed to avoid some eventual errors and warnings */
       useNewUrlParser: true,
       useUnifiedTopology: true,
