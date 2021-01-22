ea3a557473f3acad3c88fc187eec41664e15958a
adammak <aminedammak86@gmail.com>
2021-01-22 10:19:32 +0100

    Added manual acknowledge

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,13 +1,28 @@
-import nats from "node-nats-streaming";
-import { isJsxClosingFragment } from "typescript";
+import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
 console.clear();
-const stan = nats.connect("ticketing", "123", { url: "http://localhost:4222" });
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
+  url: "http://localhost:4222",
+});
 
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
-  const subscribtion = stan.subscribe("ticket:created");
-  subscribtion.on("message", (msg) => {
-    console.log("message received::::!!!!");
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+
+  const subscribtion = stan.subscribe(
+    "ticket:created",
+    "orders-service-queue-group",
+    options
+  );
+  subscribtion.on("message", (msg: Message) => {
+    const data = msg.getData();
+
+    //we need to verify the the type of data is string and not buffer before parsing it as JSON
+    if (typeof data === "string")
+      console.log(
+        `Received event ${msg.getSequence()}, with data: ${JSON.parse(data)}`
+      );
+    msg.ack();
   });
 });
