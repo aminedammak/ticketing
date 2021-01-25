e90a8f104164380d732eaa7eed4af1c0b41ec7fc
adammak <aminedammak86@gmail.com>
2021-01-25 09:43:22 +0100

    Created abstract class for listener and extend it fromt TicketCreatedListener class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
 import { randomBytes } from "crypto";
 console.clear();
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
@@ -13,28 +13,60 @@ stan.on("connect", () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName("accouting-service");
-
-  const subscribtion = stan.subscribe(
-    "ticket:created",
-    "order-service-queue-group",
-    options
-  );
-  subscribtion.on("message", (msg: Message) => {
-    const data = msg.getData();
-
-    //we need to verify the the type of data is string and not buffer before parsing it as JSON
-    if (typeof data === "string")
-      console.log(
-        `Received event ${msg.getSequence()}, with data: ${JSON.parse(data)}`
-      );
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on("SIGINT", () => stan.close());
 process.on("SIGTERM", () => stan.close());
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void;
+  private client: Stan;
+  protected ackWait = 5 * 1000;
+  constructor(client: Stan) {
+    this.client = client;
+  }
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const subscribtion = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscribtion.on("message", (msg: Message) => {
+      console.log(`Message received ${this.subject} / ${this.queueGroupName}`);
+
+      const parsedData = this.parseMessage(msg);
+
+      //We also pass msg because maybe we will need some data from it
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === "string"
+      ? JSON.parse(data)
+      : JSON.parse(data.toString("utf8"));
+  }
+}
+
+class TicketCreatedListener extends Listener {
+  subject = "ticket:created";
+  queueGroupName = "payments-service";
+  onMessage(data: any, msg: Message) {
+    console.log("Event Data!!!!", data);
+    msg.ack();
+  }
+}
