34e4544302b5b03d2eb1b0b2907ba7024a1c9c2d
adammak <aminedammak86@gmail.com>
2021-01-25 16:25:56 +0100

    Handle asynchrounous publish method

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -5,15 +5,17 @@ import { Subjects } from "./events/subjects";
 console.clear();
 const stan = nats.connect("ticketing", "abc", { url: "http://localhost:4222" });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to NATS");
 
-  const data = {
-    id: "555",
-    title: "My ticket",
-    price: 10,
-  };
-
   const publisher = new TicketCreatedPublisher(stan);
-  publisher.publish(data);
+  try {
+    await publisher.publish({
+      id: "555",
+      title: "My ticket",
+      price: 10,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
