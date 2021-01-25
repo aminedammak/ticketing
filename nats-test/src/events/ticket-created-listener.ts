61071394d49ea6e5d1ad8cdccb20134a243fdd0f
adammak <aminedammak86@gmail.com>
2021-01-25 15:36:56 +0100

    Using custom publisher

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -4,10 +4,14 @@ import { TicketCreatedEvent } from "./ticket-created-event";
 import { Subjects } from "./subjects";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-  subject = Subjects.TicketCreated;
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = "payments-service";
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message) {
     console.log("Event Data!!", data);
+    console.log(data.id);
+    console.log(data.title);
+    console.log(data.price);
+
     msg.ack();
   }
 }
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,17 +1,19 @@
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 import nats from "node-nats-streaming";
+import { Subjects } from "./events/subjects";
+
 console.clear();
 const stan = nats.connect("ticketing", "abc", { url: "http://localhost:4222" });
 
 stan.on("connect", () => {
   console.log("Publisher connected to NATS");
 
-  const data = JSON.stringify({
-    id: "123",
+  const data = {
+    id: "555",
     title: "My ticket",
     price: 10,
-  });
+  };
 
-  stan.publish("ticket:created", data, () => {
-    console.log("Event published");
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  publisher.publish(data);
 });
