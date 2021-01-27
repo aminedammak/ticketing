56ac1f824330544de63244cde4a968eff4456c20
adammak <aminedammak86@gmail.com>
2021-01-27 13:10:40 +0100

    Create the replicated Ticket model inside of the order service

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,14 @@
 import express, { Request, Response } from "express";
-import { validateRequest, requireAuth } from "@dmk_tickets/common";
+import {
+  validateRequest,
+  requireAuth,
+  NotFoundError,
+  OrderStatus,
+} from "@dmk_tickets/common";
 import { body } from "express-validator";
 import mongoose from "mongoose";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -19,7 +26,30 @@ router.post(
   async (req: Request, res: Response) => {
     const { ticketId } = req.body;
 
-    res.send({});
+    //Find the ticket the user is trying to order in the database
+    const ticketDoc = await Ticket.findById(ticketId);
+    if (!ticketDoc) {
+      throw new NotFoundError();
+    }
+
+    //Make sure that this ticket is not already reserved
+
+    //Calculate an expiration date for this order
+
+    //Build the order and save it to the database
+
+    //Publish an event saying that an order was created
+
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: new Date(new Date().getTime() + 15 * 60),
+      ticket: ticketDoc,
+    });
+
+    await order.save();
+
+    res.send(order);
   }
 );
 
