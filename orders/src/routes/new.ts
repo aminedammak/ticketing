24a8b97cd5e32a270d1e485cabcc332e676a8520
adammak <aminedammak86@gmail.com>
2021-01-27 12:05:10 +0100

    Created order model, added status enum

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,26 @@
 import express, { Request, Response } from "express";
+import { validateRequest, requireAuth } from "@dmk_tickets/common";
+import { body } from "express-validator";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketId must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
